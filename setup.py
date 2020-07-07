93fb110cb41fed45c9ddf178d5a017054808c239
R.A. Stern <rastern@users.noreply.github.com>
2020-07-07 11:20:12 -0400

    Update classifiers

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
     ],
@@ -52,5 +51,4 @@ setup(
     python_requires=">=3.6",
     install_requires=requires,
     license=about['__license__'],
-    zip_safe=False,
 )
